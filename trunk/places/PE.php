ce8b50f230bdd2a419321bd6bf8990ebf36d8784
CesarChris710 <cesarchris710@gmail.com>
2019-02-12 10:43:56 -0500

    Create PE.php
    Version 1.0.0 only places in 'Lima Metropolitana' and 'Callao'

---
+++ trunk/places/PE.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Peruvian places
+ *
+ * @author  Chris Silupú | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+global $places;
+
+$places['PE'] = array(
+	'LMA' => array(
+		'Ancon',
+		'Ate',
+		'Barranco',
+		'Breña',
+		'Carabayllo',
+		'Chaclacayo',
+		'Chorrillos',
+		'Cieneguilla',
+		'Comas',
+		'El Agustino',
+		'Independencia',
+		'Jesús María',
+		'La Molina',
+		'La Victoria',
+		'Cercado de Lima',
+		'Lince',
+		'Los Olivos',
+		'Chosica',
+		'Lurin',
+		'Magdalena del Mar',
+		'Miraflores',
+		'Pachacamac',
+		'Pucusana',
+		'Pueblo Libre',
+		'Puente Piedra',
+		'Punta Hermosa',
+		'Punta Negra',
+		'Rimac',
+		'San Bartolo',
+		'San Borja',
+		'San Isidro',
+		'San Juan de Lurigancho',
+		'San Juan de Miraflores',
+		'San Luis',
+		'San Martin de Porres',
+		'San Miguel',
+		'Santa Anita',
+		'Santa Maria del Mar',
+		'Santa Rosa',
+		'Surco',
+		'Surquillo',
+		'Villa El Salvador',
+		'Villa Maria Del Triunfo'
+	),	
+	'CAL' => array(
+		'Bellavista',
+		'Callao',
+		'Carmen De La Legua',
+		'La Perla',
+		'La Punta',
+		'Ventanilla',
+		'Mi Peru'
+	),
+	
+);
