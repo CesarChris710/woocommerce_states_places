983d0858f8d279a2bacbab607f945a8db057ea3c
CesarChris710 <cesarchris710@gmail.com>
2019-02-12 10:52:59 -0500

    Create PE.php
    Peruvian states

---
+++ trunk/states/PE.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Peruvian states
+ *
+ * @author  Chris Silupú | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states['PE'] = array(
+	'CAL' => __( 'Callao', 'woocommerce' ),
+	'LMA' => __( 'Lima Metropolitana', 'woocommerce' ),
+	'AMA' => __( 'Amazonas', 'woocommerce' ),
+	'ANC' => __( 'Ancash', 'woocommerce' ),
+	'APU' => __( 'Apur&iacute;mac', 'woocommerce' ),
+	'ARE' => __( 'Arequipa', 'woocommerce' ),
+	'AYA' => __( 'Ayacucho', 'woocommerce' ),
+	'CAJ' => __( 'Cajamarca', 'woocommerce' ),
+	'CUS' => __( 'Cusco', 'woocommerce' ),
+	'HUV' => __( 'Huancavelica', 'woocommerce' ),
+	'HUC' => __( 'Hu&aacute;nuco', 'woocommerce' ),
+	'ICA' => __( 'Ica', 'woocommerce' ),
+	'JUN' => __( 'Jun&iacute;n', 'woocommerce' ),
+	'LAL' => __( 'La Libertad', 'woocommerce' ),
+	'LAM' => __( 'Lambayeque', 'woocommerce' ),
+	'LIM' => __( 'Lima Provincias', 'woocommerce' ),
+	'LOR' => __( 'Loreto', 'woocommerce' ),
+	'MDD' => __( 'Madre de Dios', 'woocommerce' ),
+	'MOQ' => __( 'Moquegua', 'woocommerce' ),
+	'PAS' => __( 'Pasco', 'woocommerce' ),
+	'PIU' => __( 'Piura', 'woocommerce' ),
+	'PUN' => __( 'Puno', 'woocommerce' ),
+	'SAM' => __( 'San Mart&iacute;n', 'woocommerce' ),
+	'TAC' => __( 'Tacna', 'woocommerce' ),
+	'TUM' => __( 'Tumbes', 'woocommerce' ),
+	'UCA' => __( 'Ucayali', 'woocommerce' ),
+);
