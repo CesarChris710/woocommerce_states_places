21a8d37253590d4ce56a09bf83963a770720f726
Chris Silupu <cesarchris710@gmail.com>
2020-01-11 19:32:36 -0500

    Create PE.php

---
+++ trunk/states/PE.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Peruvian states
+ *
+ * @author  Chris Silupú | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+ 
+global $states;
+
+$states['PE'] = array(
+	'CAL' => 'Callao',
+	'LMA' => 'Lima Metropolitana',
+	'AMA' => 'Amazonas',
+	'ANC' => 'Ancash',
+	'APU' => 'Apur&iacute;mac',
+	'ARE' => 'Arequipa',
+	'AYA' => 'Ayacucho',
+	'CAJ' => 'Cajamarca',
+	'CUS' => 'Cusco',
+	'HUV' => 'Huancavelica',
+	'HUC' => 'Hu&aacute;nuco',
+	'ICA' => 'Ica',
+	'JUN' => 'Jun&iacute;n',
+	'LAL' => 'La Libertad',
+	'LAM' => 'Lambayeque',
+	'LIM' => 'Lima Provincias',
+	'LOR' => 'Loreto',
+	'MDD' => 'Madre de Dios',
+	'MOQ' => 'Moquegua',
+	'PAS' => 'Pasco',
+	'PIU' => 'Piura',
+	'PUN' => 'Puno',
+	'SAM' => 'San Mart&iacute;n',
+	'TAC' => 'Tacna',
+	'TUM' => 'Tumbes',
+	'UCA' => 'Ucayali',
+);
